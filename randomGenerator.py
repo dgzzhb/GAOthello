32f5b5f67cdd412653455111f36a9c0d41bdee97
Tian Zheng <tz5318@truman.edu>
2013-11-25 10:46:40 -0600

    add comment

--- randomGenerator.py
+++ randomGenerator.py
@@ -1,7 +1,10 @@
+#Since we cannot generate first generation using GA, this random generator works on it
+
 from random import randint
 
 f = open('firstGeneration', 'w')
 
+# Generate 20 lines with an array of 16 random numbers in each line
 for i in xrange(20):
 	board = []
 	for j in xrange(16):
